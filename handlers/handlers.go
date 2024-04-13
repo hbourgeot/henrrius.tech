03f53a31819de3c4fde916416ef36b0350b419fb
henrrius <henrrius@henrrius>
2024-04-13 17:45:53 -0400

    fixing some things

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,17 @@
 package handlers
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/hbourgeot/blog/helpers"
+	"github.com/hbourgeot/blog/views"
+	"github.com/hbourgeot/blog/views/layout"
+	partial "github.com/hbourgeot/blog/views/partials"
+)
 
 func Home(c fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+	return helpers.Render(c, layout.Base(views.Index()))
+}
+
+func Foo(c fiber.Ctx) error {
+	return helpers.Render(c, layout.Base(partial.Foo()))
 }
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"github.com/a-h/templ"
 	"github.com/hbourgeot/blog/handlers"
+	"github.com/hbourgeot/blog/helpers"
+	view "github.com/hbourgeot/blog/views"
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v3"
 )
@@ -14,8 +18,26 @@ func main() {
 		AppName:       "henrrius.tech Blog, a blog for devs",
 	})
 
-	app.Get("/", handlers.Home)
+	MountRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 
+}
+
+func NotFoundMiddleware(c fiber.Ctx) error {
+	return helpers.Render(c, view.NotFound(), templ.WithStatus(http.StatusNotFound))
+}
+
+func MountRoutes(app *fiber.App) {
+	// static files
+	app.Static("/static/", "./static", fiber.Static{
+		Browse: true,
+	})
+
+	// routes
+	app.Get("/", handlers.Home)
+	app.Get("/foo", handlers.Foo)
+
+	// middlewares
+	app.Use(NotFoundMiddleware)
 }
